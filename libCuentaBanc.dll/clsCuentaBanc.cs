a41ff5fb71d29283ce7d872bfe7826c794e20e25
Valenciasebas4 <valenciasebastian80@gmail.com>
2026-02-20 09:18:59 -0500

    Created attributes

--- libCuentaBanc.dll/clsCuentaBanc.cs
+++ libCuentaBanc.dll/clsCuentaBanc.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace libCuentaBanc.dll
 {
-    public class Class1
+    public abstract class clsCuenta
     {
+        #region Atributos
+        // se usa protected para los atributos de la clase que va a ser padre y los hijos puedan heredar
+        protected int intNroCta;
+        protected string strFecCreac;
+        protected int intTipoDoc;
+        protected int intNroDcto;
+        protected string strTitular;
+        protected float fltSaldo;
+        protected string rutaFile;
+        protected string strError;
+
+        #endregion
+
+
+
     }
 }
