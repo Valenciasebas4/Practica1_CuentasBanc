3a424423c0781c1d83cf9b053a7d1e82c320ebaa
Valenciasebas4 <valenciasebastian80@gmail.com>
2026-02-20 10:04:07 -0500

    Class clsGenerales Created, methods UltimoId and leerArchivo Created

--- libCuentaBanc.dll/clsCuentaBanc.cs
+++ libCuentaBanc.dll/clsCuentaBanc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,6 +53,69 @@ namespace libCuentaBanc.dll
         #region Métodos
         public abstract bool Deposito(int nroCta, float valor);
         public abstract bool Retiro(int nroCta, float valor);
+        #endregion
+    }
+
+    public class clsGenerales 
+    {
+        #region Métodos
+        public int UltimoId(string ruta)
+        {
+            int rpta = -1;
+            string carpeta = Path.GetDirectoryName(ruta);
+
+            if (!Directory.Exists(carpeta))
+                Directory.CreateDirectory(carpeta);
+            if (File.Exists(ruta))
+            {
+                // 1. Leer todas las lineas del archivo
+                string[] lineas = File.ReadAllLines(ruta);
+                //2. Verificar si hay lineas en el archivo
+                if (lineas.Length > 0)
+                {
+                    //Opción 3: Obtener el último registro
+                    string ultimoRegistro = lineas.LastOrDefault();
+                    string[] datos = ultimoRegistro.Split(',');
+                    rpta = Convert.ToInt32(datos[0]);
+                }
+                else
+                    rpta = 0;
+            }
+            return rpta;
+        }
+
+        public List<string> leerArchivo(string ruta, int nroCta)
+        {
+            List<string> rpta = new List<string>();
+            try
+            {
+                string carpeta = Path.GetDirectoryName(ruta);
+                // Verificamos si la carpeta existe, o si no, se crea
+                if (!Directory.Exists(carpeta))
+                    Directory.CreateDirectory(carpeta);
+                // Verificamos si el archivo existe, o si no, se crea
+                if (!File.Exists(ruta))
+                    return rpta;
+                string[] lineas = File.ReadAllLines(ruta);
+                if (lineas.Length > 0)
+                {
+                    foreach (string rgtro in lineas)
+                    {
+                        string[] datos = rgtro.Split(':');
+                        if (int.Parse(datos[0]) == nroCta)
+                        {
+                            rpta.AddRange(datos);
+                            break;
+                        }
+                    }
+                }
+                return rpta;
+            }
+            catch
+            {
+                return rpta;
+            }
+        }
         #endregion
     }
 }
